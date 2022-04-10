4e343ac2b9f59b2dc9f493f9db16cd5e36dc4a6e
Ivan Miranda Ortega <ivanmirandaortega@Ivans-MBP-27.lan>
2022-04-09 19:20:44 -0700

    added an index and error page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
-var router = require('express').Router();
+const router = require('express').Router();
 const passport = require('passport');
 
 // The root route renders our only view
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   // Where do you want to go for the root route
   // in the student demo this was res.redirect('/students'), what do you want?
   // This could be a landing page, or just redirect to your main resource page which you'll have an a tag that makes 
   // a request to `/auth/google` route below
+  res.render('index', { title: 'Welcome to share' });
 });
 
 // Google OAuth login route
@@ -19,13 +20,13 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/', // where do you want the client to go after you login 
-    failureRedirect : '/' // where do you want the client to go if login fails
+    successRedirect: '/', // where do you want the client to go after you login 
+    failureRedirect: '/' // where do you want the client to go if login fails
   }
 ));
 
 // OAuth logout route
-router.get('/logout', function(req, res){
+router.get('/logout', function (req, res) {
   req.logout();
   res.redirect('/');
 });
--- server.js
+++ server.js
@@ -53,8 +53,24 @@ app.use('/', indexRoutes);
 
 
 // invalid request, send 404 page
-app.use(function(req, res) {
-  res.status(404).send('Cant find that!');
+// app.use(function(req, res) {
+//   res.status(404).send('Cant find that!');
+// });
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
