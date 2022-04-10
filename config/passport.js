93894b1210c6316bee1800adb5f969468e4e236d
Ivan Miranda Ortega <ivanmirandaortega@Ivans-MBP-28.lan>
2022-04-09 21:08:34 -0700

    successfully linked oauth

--- config/passport.js
+++ config/passport.js
@@ -1,30 +1,47 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 //Require your User Model here!
+const User = require('../models/user');
 
 // configuring Passport!
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // a user has logged in via OAuth!
-    // refer to the lesson plan from earlier today in order to set this up
-
-  }
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK
+}, function (accessToken, refreshToken, profile, cb) {
+  // a user has logged in via OAuth!
+  console.log(profile, ' <- profile')
+  User.findOne({ googleId: profile.id }, function (err, user) {
+    if (user) return cb(null, user);
+    if (err) return cb(err)
+    User.create({
+      name: profile.displayName,
+      googleId: profile.id,
+      email: profile.emails[0].value,
+      avatar: profile.photos[0].value
+    }, function (err, createdUser) {
+      if (createdUser) return cb(null, createdUser)
+      if (err) return cb(console.error)
+    })
+  })
+}
 ));
 
-passport.serializeUser(function(user, done) {
+
+
+passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
+passport.deserializeUser(function (userId, done) {
 
   // Find your User, using your model, and then call done(err, whateverYourUserIsCalled)
   // When you call this done function passport assigns the user document to req.user, which will 
   // be availible in every Single controller function, so you always know the logged in user
-
+  User.findById(userId, function (err, user) {
+    if (err) return done(err);
+    done(null, user);
+  })
 });
 
 
