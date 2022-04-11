f90b949945f9c92f97e4ed9fb9b303ae2b9fab31
Ivan Miranda Ortega <ivanmirandaortega@Ivans-MBP.lan>
2022-04-10 20:31:15 -0700

    adjusted my routes

--- controllers/thoughts.js
+++ controllers/thoughts.js
@@ -1,15 +1,42 @@
 const Thought = require('../models/thoughts');
 
 module.exports = {
-    index
+    index,
+    show,
+    create,
+    new: newThought
 };
 
-function index(req, res) {
-    console.log(req.user, ' <- req.user')
-    Thought.find({}, function (err, thoughts) {
+function show(req, res) {
+    Thought.findById(req.params.id, function (err, thought) {
+        console.log(thought, ' <- thought')
         res.render('thoughts/index', {
-            thoughts,
+            thought,
             title: "Feed"
         })
     })
-}
+};
+
+function newThought(req, res) {
+    res.render('thoughts/new', { title: 'Share A Thought' })
+};
+
+function index(req, res) {
+    // console.log(req.user, ' <- req.user')
+    Thought.find({}, function (err, thought) {
+        console.log(thought, ' <- not sure ')
+        res.render('thoughts/index', {
+            thought,
+            title: "Feed"
+        })
+    })
+};
+
+function create(req, res) {
+    const thought = new Thought(req.body)
+    thought.save(function (err) {
+        if (err) return
+        console.log(thought, ' <- this is a new thought')
+        res.redirect('/feed')
+    })
+};
--- routes/thoughts.js
+++ routes/thoughts.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const thoughtsController = require('../controllers/thoughts');
 const isLoggedIn = require('../config/auth');
 
+// GET /feed
 router.get('/', isLoggedIn, thoughtsController.index);
+// GET /feed
+router.get('/thoughts/new', isLoggedIn, thoughtsController.new);
+
+// POST /feed
+router.post('/', thoughtsController.create);
+
 
 
 module.exports = router;
